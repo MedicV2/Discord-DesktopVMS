25d66f411fa995b7cdee1ca21e504797664c0778
Medic <99144047+MedicV2@users.noreply.github.com>
2023-06-12 13:53:20 +0200

    Update waveform.js

--- audio-processing/waveform.js
+++ audio-processing/waveform.js
@@ -1,81 +1,72 @@
-const fs = require('fs').promises;
-const wav = require('node-wav');
-const ffmpegPath = require('ffmpeg-static');
+const fs = require('fs');
+const path = require('path');
 const ffmpeg = require('fluent-ffmpeg');
-const tmp = require('tmp-promise');
+const wavDecoder = require('wav-decoder');
 
-ffmpeg.setFfmpegPath(ffmpegPath);
+module.exports = async function calculateWaveform(audioFilePath,duration) {
+  const outputFolder = path.join(__dirname, '../vms/customaudio/');
+  const tempWavFile = path.join(outputFolder, 'temp.wav');
 
-// Just for logging purposes
-const discordsAmplitudes = [
-  0, 146, 0, 21, 34, 0, 0, 0, 0, 0, 0, 0, 0, 30, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 187, 183, 179, 118, 103, 139, 41, 23, 20, 5, 26, 0, 195, 174, 197, 126, 194, 146, 171, 190, 182, 193, 186, 179, 169, 169, 160, 148, 90, 137, 143, 181, 166, 115, 103, 181, 181, 180, 160, 162, 152, 160, 164, 66, 59, 95, 69, 36, 18, 13, 16, 3, 17, 0, 0, 0, 9, 14, 21, 4, 0
-];
+  try {
+    await new Promise((resolve, reject) => {
+      ffmpeg(audioFilePath)
+        .audioCodec('pcm_s16le')
+        .audioChannels(1)
+        .audioFrequency(48000)
+        .format('wav')
+        .on('error', (err) => {
+          console.error(`Error converting audio to WAV: ${err.message}`);
+          reject(err);
+        })
+        .on('end', () => {
+          console.log('Audio converted to WAV successfully.');
+          resolve();
+        })
+        .save(tempWavFile);
+    });
 
-//Encodes an array of amplitudes to a Base64 string.
-function encodeAmplitudesToBase64(deciArr) {
-  return Buffer.from(deciArr.map(c => String.fromCharCode(c)).join(''), 'binary').toString('base64');
-}
+    // Read the WAV data
+    const buffer = fs.readFileSync(tempWavFile);
+    const decodedData = await wavDecoder.decode(buffer);
+    //decodedData.channelData[0].length gives the total number of samples in the audio
+    const audioDuration = decodedData.channelData[0].length / decodedData.sampleRate;
+    console.log("AUDIO DURATION: "+audioDuration)
 
-async function convertToWav(inputBuffer) {
-  const { path: inputPath, cleanup: cleanupInput } = await tmp.file();
-  const { path: outputPath, cleanup: cleanupOutput } = await tmp.file({ postfix: '.wav' });
+    // Calculate the number of chunks
+    const numChunks = Math.min(Math.max(Math.ceil(audioDuration * audioDuration), 5), 256);
 
-  await fs.writeFile(inputPath, inputBuffer);
+    const waveform = [];
+    const samplesPerChunk = Math.ceil(decodedData.channelData[0].length / numChunks);
 
-  return new Promise((resolve, reject) => {
-    ffmpeg(inputPath)
-      .output(outputPath)
-      .on('end', async () => {
-        const outputBuffer = await fs.readFile(outputPath);
-        cleanupInput();
-        cleanupOutput();
-        resolve(outputBuffer);
-      })
-      .on('error', err => {
-        cleanupInput();
-        cleanupOutput();
-        reject(err);
-      })
-      .run();
-  });
-}
+    for (let i = 0; i < numChunks; i++) {
+      let max = 0;
+      for (let j = 0; j < samplesPerChunk; j++) {
+        if (i * samplesPerChunk + j < decodedData.channelData[0].length) {
+          max = Math.max(max, Math.abs(decodedData.channelData[0][i * samplesPerChunk + j]));
+        }
+      }
+      waveform.push(max); // Store the maximum amplitude value without scaling
+    }
 
-async function getNormalizedAmplitudes(filepath, numSegments) {
-  // Load the audio file
-  const audioBuffer = await fs.readFile(filepath);
-  const wavBuffer = await convertToWav(audioBuffer);
-  const audioData = wav.decode(wavBuffer);
+    // Normalize the waveform values
+    const maxAmplitude = Math.max(...waveform);
+    const normalizedWaveform = waveform.map(value => value / maxAmplitude);
 
-  // Compute the number of samples and sample length
-  const numSamples = audioData.channelData[0].length;
-  const segmentLength = Math.floor(numSamples / numSegments);
+    const scaledWaveform = normalizedWaveform.map(value => Math.round(value * 200));
 
-  // Initialize the array to store the normalized amplitudes
-  const amplitudes = new Array(numSegments);
+    const myWaveform = btoa(scaledWaveform.map(c => String.fromCharCode(c)).join(''));
+    console.log(scaledWaveform);
+    console.log(scaledWaveform.length);
+    console.log(myWaveform);
 
-  // Compute the average amplitude for each segment
-  for (let i = 0; i < numSegments; i++) {
-    const segmentStart = i * segmentLength;
-    const segmentEnd = (i + 1) * segmentLength;
-    const segment = audioData.channelData[0].slice(segmentStart, segmentEnd);
-    const amplitude = segment.reduce((sum, value) => sum + Math.abs(value), 0) / segment.length;
-    amplitudes[i] = amplitude;
+    // Remove the temporary WAV file
+    fs.unlinkSync(tempWavFile);
+
+    return myWaveform;
+  } catch (error) {
+    console.error('Error calculating waveform:', error);
+    throw error;
   }
+};
 
-  // Normalize the amplitudes to fit within the range of 0 to 255 (Discords limit on voice messages is 200 prob because of noise canceling/suppression.)
-  const maxAmplitude = Math.max(...amplitudes);
-  const normalizedAmplitudes = amplitudes.map(a => Math.round(a / maxAmplitude * 255));
-
-  return normalizedAmplitudes;
-}
-
-getNormalizedAmplitudes('../vms/customaudio/voice-message.ogg', 89)
-  .then(amplitudes => {
-    console.log('\nNormalized Amplitudes:', amplitudes);
-    console.log('\nDiscord\'s Amplitudes:', discordsAmplitudes);
-    console.log('Custom: \n', encodeAmplitudesToBase64(amplitudes));
-    console.log('Discord: \n', encodeAmplitudesToBase64(discordsAmplitudes));
-  })
-  .catch(err => console.error(err));
-
-  //Scuffed version of calculating the waveform data, trying to make it more accurate.
+//SCUFFED TERRIBLE WAVEFORM CALCULATION SINCE I GAVE UP ON TRYING TO DO IT CORRECTLY.
